26baf122055c862d970418f4abe06672efed7a2e
khun <woghd567@naver.com>
2022-06-13 23:12:37 +0900

    FEAT: useCanvas Hook 추가

--- pages/CanvasAnimation.tsx
+++ pages/CanvasAnimation.tsx
@@ -1,31 +1,14 @@
 import React, { RefObject, useEffect, useRef } from 'react';
 
+import useCanvas from '../hooks/useCanvas';
+
 interface ICanvasAnimationProps {
   canvasWidth: number;
   canvasHeight: number;
 }
 
 const CanvasAnimation: React.FC<ICanvasAnimationProps> = ({ canvasWidth, canvasHeight }) => {
-  const canvasRef: RefObject<HTMLCanvasElement> = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas?.getContext('2d');
-
-    const setCanvas = () => {
-      const devicePixelRatio = window.devicePixelRatio ?? 1;
-
-      if (canvas && ctx) {
-        canvas.style.width = canvasWidth + 'px';
-        canvas.style.height = canvasHeight + 'px';
-        canvas.width = canvasWidth * devicePixelRatio;
-        canvas.height = canvasHeight * devicePixelRatio;
-
-        ctx.scale(devicePixelRatio, devicePixelRatio);
-      }
-    };
-    setCanvas();
-  }, [canvasWidth, canvasHeight]);
+  const canvasRef: RefObject<HTMLCanvasElement> = useCanvas(canvasWidth, canvasHeight);
 
   return <canvas ref={canvasRef} />;
 };
