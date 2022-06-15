fd6962ab5d126ca7cb81898719c2e7ae456be708
khun <woghd567@naver.com>
2022-06-15 23:30:45 +0900

    FEAT: pointLine 추가

--- pages/CanvasAnimation.tsx
+++ pages/CanvasAnimation.tsx
@@ -3,6 +3,7 @@ import React, { RefObject, useEffect, useRef } from 'react';
 import useCanvas from '../hooks/useCanvas';
 
 import { ILightSource, LightSource } from '../utils/lightSource';
+import { IPointLine, PointLine } from '../utils/pointLine';
 
 interface ICanvasAnimationProps {
   canvasWidth: number;
@@ -15,12 +16,29 @@ const CanvasAnimation: React.FC<ICanvasAnimationProps> = ({ canvasWidth, canvasH
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
   };
 
+  let points: IPointLine[] = [];
+  const initPoints = () => {
+    const POINT_NUMBER = 72;
+    const POINT_GAP = canvasWidth / POINT_NUMBER;
+    for (let i = 0; i <= POINT_NUMBER; i++) {
+      const point: IPointLine = new PointLine(POINT_GAP, i, canvasWidth, canvasHeight);
+      points.push(point);
+    }
+  };
+  if (canvasWidth !== 0 && canvasHeight !== 0) {
+    initPoints();
+  }
   const lightSource: ILightSource = new LightSource(canvasWidth, canvasHeight);
 
   const animate = (ctx: CanvasRenderingContext2D) => {
     fillBackground(ctx);
     lightSource.drawGradation(ctx);
     lightSource.drawLightSource(ctx);
+
+    for (const point of points) {
+      lightSource.drawLightLines(ctx, point.pointCenterX, point.pointCenterY);
+      point.animate(ctx);
+    }
   };
 
   console.log(lightSource);
